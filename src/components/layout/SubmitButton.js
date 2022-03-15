e20430cbe70baf964bc50d34f996750faa8bf357
Renato Azevedo <renato.azevedo0001@gmail.com>
2022-03-15 10:48:43 -0300

    a

--- src/components/layout/SubmitButton.js
+++ src/components/layout/SubmitButton.js
@@ -1,6 +1,6 @@
 import styles from './SubmitButton.module.css'
 
-function Submit({text}) {
+function SubmitButton({text}) {
     return(
             <div>
                 <button className={styles.btn}>{text}</button>

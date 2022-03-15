b1675144eef5ea844cca168f85544d552bda1990
Renato Azevedo <renato.azevedo0001@gmail.com>
2022-03-15 08:54:49 -0300

    aaa

--- src/components/layout/Form.js
+++ src/components/layout/Form.js
@@ -2,9 +2,11 @@ import styles from './Form.module.css'
 import InputForm from './InputForm.js'
 function Form() {
     return(
+        <div>
         <form className={styles.formulario}>
             <InputForm nome="projeto1" type="text" project="Nome do Projeto" id="projeto1" placeholder="Insira o nome do Projeto"  />
         </form>
+        </div>
     )
 }
 export default Form
